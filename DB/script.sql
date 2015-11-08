a6e79aeeca318a53f3d4047b4b8debdfa24701f0
Cássio Ferrazzo <cassioferrazzo@gmail.com>
2015-11-08 21:41:26 -0200

    adicionado a estrutura do banco e as anotações de documentação

---
+++ DB/script.sql
@@ -0,0 +1,29 @@
+/*
+Created		28/10/2015
+Modified		28/10/2015
+Project		
+Model			
+Company		
+Author		
+Version		
+Database		PostgreSQL 8.1 
+*/
+
+
+/* Create Tables */
+
+
+Create table "tb_fila"
+(
+	"id_fila" Serial NOT NULL,
+	"ativo" Boolean,
+	"codigo" Varchar(10),
+	"data" DATE NOT NULL,
+	"hora" TIME NOT NULL
+ primary key ("id_fila")
+) Without Oids;
+
+
+
+
+
